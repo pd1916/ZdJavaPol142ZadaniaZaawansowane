28cb751adccab7290ed28663c787adf13f56b96a
Pawel Dudko <paweldudko@outlook.com>
2023-11-05 13:48:36 +0100

    solution task17

--- src/main/java/pl/sdacademy/java/advanced/exercises/day2/task17/Task17.java
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task17/Task17.java
@@ -6,5 +6,9 @@ public class Task17 {
     public static void main(String[] args) {
         double result = MeasurementConverter.convert(1, INCHES_TO_CENTIMETERS);
         System.out.println(result);
+
+
+        double resultFunction = MeasurementConverter.convert(5, ConversionTypeWithFunction.METERS_TO_YARDS);
+        System.out.println(resultFunction);
     }
 }
