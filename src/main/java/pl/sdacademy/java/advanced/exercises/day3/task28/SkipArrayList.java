6c8d9583a49dfb7ada9d87085d3be2b888609698
Pawel Dudko <paweldudko@outlook.com>
2023-11-18 11:06:42 +0100

    solution task28

--- src/main/java/pl/sdacademy/java/advanced/exercises/day3/task28/SkipArrayList.java
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day3/task28/SkipArrayList.java
@@ -7,14 +7,14 @@ public class SkipArrayList<E> extends ArrayList<E> {
 
     public List<E> getEveryNthElement(int startIndex, int skip) {
         List<E> result = new ArrayList<>();
-//        for(int i = startIndex; i < size(); i=+skip) {
-//
-//        }
-        int currentIndex = startIndex;
-        while(currentIndex < size()) {
+        for(int currentIndex = startIndex; currentIndex < size(); currentIndex+=skip+1) {
             result.add(this.get(currentIndex));
-            currentIndex += skip + 1;
         }
+//        int currentIndex = startIndex;
+//        while(currentIndex < size()) {
+//            result.add(this.get(currentIndex));
+//            currentIndex += skip + 1;
+//        }
         return result;
     }
 }
